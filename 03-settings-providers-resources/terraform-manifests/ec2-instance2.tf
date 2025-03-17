a1e591aa83b550793d0b39f8026be669e7032964
Damilola Ajayi <ajay0006@algonquinlive.com>
2025-03-17 12:07:42 -0400

    added the vpc resource block to create a vpc with a private ip address range

--- 03-settings-providers-resources/terraform-manifests/ec2-instance2.tf
+++ 03-settings-providers-resources/terraform-manifests/ec2-instance2.tf
@@ -1,3 +1,8 @@
+resource "aws_vpc" "MyVpc" { # this creates a VPC with the following private ip address range
+    cidr_block = "10.1.0.0/16"
+  
+}
+
 resource "aws_instance" "ec2demo2" {
     ami = "ami-04aa00acb1165b32a"
     instance_type = "t2.micro"
