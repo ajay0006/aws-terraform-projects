598af95e606363426e942efb5ce199964e9a641e
Damilola Ajayi <ajay0006@algonquinlive.com>
2025-03-17 12:19:59 -0400

    added the aws subnet block, created 2 private and public subnets each

--- 03-settings-providers-resources/terraform-manifests/ec2-instance2.tf
+++ 03-settings-providers-resources/terraform-manifests/ec2-instance2.tf
@@ -1,5 +1,36 @@
 resource "aws_vpc" "MyVpc" { # this creates a VPC with the following private ip address range
-    cidr_block = "10.1.0.0/16"
+    cidr_block = "10.0.0.0/16"  
+}
+
+resource "aws_subnet" "Public-1A" {
+    vpc_id = aws_vpc.Public-1A
+    availability_zone = "us-east-1a"
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+  
+}
+
+resource "aws_subnet" "Public-1B" {
+    vpc_id = aws_vpc.Public-1B
+    availability_zone = "us-east-1b"
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = true
+  
+}
+
+resource "aws_subnet" "Private-1A" {
+    vpc_id = aws_vpc.Private-1A
+    availability_zone = "us-east-1a"
+    cidr_block = "10.0.3.0/24"
+    map_public_ip_on_launch = false
+  
+}
+
+resource "aws_subnet" "Private-1B" {
+    vpc_id = aws_vpc.Private-1A
+    availability_zone = "us-east-1b"
+    cidr_block = "10.0.4.0/24"
+    map_public_ip_on_launch = false
   
 }
 
